1604562c02518feae55b04893e2a3c22af92342f
shinebayar <shinebayar999@gmail.com>
2022-05-17 11:46:48 +0800

    end of blog example

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  console.log('dddddddddddddddddddddd: ', Component, pageProps)
+  // console.log('dddddddddddddddddddddd: ', Component, pageProps)
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from 'next/link';
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,6 +15,8 @@ export async function getStaticProps() {
 }
 
 export default function Home({allPostsData}) {
+  // console.log('BBBBBBBBBBBBBBBBBBBBB: ', typeof(allPostsData));
+  // console.log('BBBBBBBBBBBBBBBBBBBBB: ', allPostsData);
   return (
     <Layout home>
       <Head>
@@ -31,11 +35,13 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
